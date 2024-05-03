bf7c8cf8751965eb67a380d646fb4473874af079
Mykola <mykola22dubyna@gmail.com>
2024-05-03 17:07:22 +0300

    Add djinni_analysis.py
    - add feature to display bar most common technologies
    - add feature to display correlation

---
+++ app/analysis/djinni_analysis.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def read_data(filename):
+    return pd.read_csv(filename)
+
+
+def get_most_common_technologies(path_to_csv: str):
+    df = read_data(path_to_csv)
+    df_common = pd.DataFrame(Counter(" ".join(df["description"]).split()).most_common(20))
+    df_common.set_index(0, inplace=True)
+    df_common.plot(kind="bar", title="The Popularity of Technologies", xlabel="Technologies", ylabel="Count", rot=60)
+    plt.show()
+
+
+def get_correlation_matrix(path_to_csv: str) -> None:
+    df = read_data(path_to_csv)
+    print(df[["experience", "views", "applications"]].corr())
